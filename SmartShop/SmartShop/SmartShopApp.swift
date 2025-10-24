d328d50fa6c80ec37d3d7683f28c68f46da5bd64
Autthawigorn <twiggyramirez8@gmail.com>
2025-10-24 21:01:11 +0700

    .

---
+++ SmartShop/SmartShop/SmartShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShopApp.swift
+//  SmartShop
+//
+//  Created by Autthawigorn Yortpiboot on 20/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct SmartShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
